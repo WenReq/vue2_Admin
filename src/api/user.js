e22c185bb2393af04fac4c6b2f65635e63eced1f
wenreq <294491328@qq.com>
2022-08-08 14:31:09 +0800

    ✨feat:更换用户图像

--- src/api/user.js
+++ src/api/user.js
@@ -16,6 +16,10 @@ export function updateUserInfo(data, config) {
   return httpPost('/my/updateUserInfo', data, config)
 }
 
-export function updatePwd(data, config) {
-  return httpPost('/my/updatePwd', data, config)
+export function updatePwd(data) {
+  return httpPost('/my/updatePwd', data)
+}
+
+export function updateAvatar(data) {
+  return httpPost('/my/update/avatar', data)
 }
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -19,7 +19,8 @@
               <span class="username"> {{ nickname }} </span>
               <el-dropdown trigger="click">
                 <div class="avatar">
-                  <img src="@/assets/img/avatar.gif" alt="avatar" srcset="" />
+                  <img v-if="userInfoForm.user_pic" :src="userInfoForm.user_pic" alt="avatar">
+                  <img v-else src="@/assets/img/avatar.gif" alt="avatar" srcset="" />
                 </div>
                 <el-dropdown-menu slot="dropdown">
                   <el-dropdown-item @click.native="handleUserInfo">个人资料</el-dropdown-item>
@@ -86,6 +87,19 @@
     <!-- 个人资料 -->
     <el-dialog title="个人资料" width="45%" :visible.sync="userInfoVisibility" >
       <el-form :model="userInfoForm" status-icon :rules="userInfoRules" ref="userInfoForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="头像">
+          <el-upload
+            :size="46"
+            :limit="1"
+            class="avatar-uploader"
+            action=""
+            :on-progress="getFile"
+            accept="image/jpeg"
+          >
+            <img v-if="userInfoForm.user_pic" :src="userInfoForm.user_pic" ref="avatarImage" alt="avatar" class="avatar" style="width: 100px;height: 100px;border-radius: 50%;">
+            <img style="width: 100px;height: 100px;border-radius: 50%;" v-else src="@/assets/img/avatar.gif" alt="avatar">
+          </el-upload>
+        </el-form-item>
         <el-form-item label="用户名">
           <el-input v-model="userInfoForm.username" readonly autocomplete="off" placeholder="请输入用户名"></el-input>
         </el-form-item>
@@ -105,7 +119,7 @@
 </template>
 
 <script>
-import { userinfo, updateUserInfo, updatePwd } from '@/api/user'
+import { userinfo, updateUserInfo, updatePwd, updateAvatar } from '@/api/user'
 import Aside from './aside.vue'
 export default {
   name: 'layout',
@@ -293,7 +307,54 @@ export default {
     },
     pathBugFun (val) {
       this.selectMenu = val
-    }
+    },
+    getFile(event, file) {
+      const vm = this
+      // 检查文件大小及格式
+      const isJPG = file.raw.type === 'image/jpeg';
+      const isLt2M = file.raw.size / 1024 / 1024 < 2;
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      if(isJPG && isLt2M){
+        debugger
+        vm.getBase64(file.raw).then(res => {
+          vm.handleSetAvatar({ avatar:res })
+        })
+      }
+    },
+    getBase64(file) {
+      return new Promise(function(resolve, reject) {
+        let reader = new FileReader();
+        let imgResult = "";
+        reader.readAsDataURL(file);
+        reader.onload = function() { 
+          imgResult = reader.result;
+        };
+        reader.onerror = function(error) { 
+          reject(error);
+        };
+        reader.onloadend = function() { 
+          resolve(imgResult);
+        };
+      })
+    },
+    handleSetAvatar(obj) {
+      let params = new URLSearchParams();
+      params.append('avatar', obj.avatar);
+      updateAvatar(params).then(res => {
+        const { status, message } = res
+        if (status === 0) {
+          this.$message.success(message)
+          this.getUserInfo()
+        } else {
+          this.$message.error(message)
+        }
+      })
+    },
   }
 }
 </script>
